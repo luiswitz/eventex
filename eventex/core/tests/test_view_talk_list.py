5f7f769944c7c785d65a259eaab1b2423007d3ca
Luis Henrique Witz <luiswitz@gmail.com>
2017-06-24 19:45:53 -0300

    Add tests for talk lists

---
+++ eventex/core/tests/test_view_talk_list.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from django.shortcuts import resolve_url as r
+
+class TalkListGet(TestCase):
+    def setUp(self):
+        self.resp = self.client.get(r('talk_list'))
+
+    def test_get(self):
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.resp, 'core/talk_list.html')
