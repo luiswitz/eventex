c4abe05c2533f04ab94a43c60fecd46be9fe0dee
Luis Henrique Witz <luiswitz@gmail.com>
2017-06-26 11:54:53 -0300

    Add talks

--- eventex/core/tests/test_view_home.py
+++ eventex/core/tests/test_view_home.py
@@ -36,3 +36,9 @@ class HomeTest(TestCase):
     def test_speakers_link(self):
         expected = 'href="{}#speakers"'.format(r('home'))
         self.assertContains(self.response, expected)
+
+
+    def test_talk_link(self):
+        expected = 'href="{}"'.format(r('talk_list'))
+        self.assertContains(self.response, expected)
+
