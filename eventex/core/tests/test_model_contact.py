6165d171985fc61843adea0f274bb3001044bf5f
Luis Henrique Witz <luiswitz@gmail.com>
2017-06-29 14:51:18 -0300

    Implement managers

--- eventex/core/tests/test_model_contact.py
+++ eventex/core/tests/test_model_contact.py
@@ -15,12 +15,10 @@ class ContactModelTest(TestCase):
 
         self.assertTrue(Contact.objects.exists())
 
-
     def test_phone(self):
         contact = Contact.objects.create(speaker = self.speaker, kind=Contact.PHONE, value='21-99999-9999')
 
         self.assertTrue(Contact.objects.exists())
-
 
     def test_choices(self):
         """ Contact kind should be limited to E or P """
@@ -28,7 +26,28 @@ class ContactModelTest(TestCase):
 
         self.assertRaises(ValidationError, contact.full_clean)
 
-
     def test_str(self):
         contact = Contact(speaker = self.speaker, kind=Contact.EMAIL, value='john@mayer.com')
         self.assertEqual('john@mayer.com', str(contact))
+
+
+class ContactManagerTest(TestCase):
+    def setUp(self):
+        s = Speaker.objects.create(
+            name='Henrique Bastos',
+            slug='henrique-bastos',
+            photo='http://hbn.link/hb-pic'
+        )
+
+        s.contact_set.create(kind=Contact.EMAIL, value='henrique@bastos.net')
+        s.contact_set.create(kind=Contact.PHONE, value='21-99999-9999')
+
+    def test_emails(self):
+        qs = Contact.objects.emails()
+        expected = ['henrique@bastos.net']
+        self.assertQuerysetEqual(qs, expected, lambda o: o.value)
+
+    def test_phones(self):
+        qs = Contact.objects.phones()
+        expected = ['21-99999-9999']
+        self.assertQuerysetEqual(qs, expected, lambda o: o.value)
